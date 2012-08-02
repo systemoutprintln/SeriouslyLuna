631dfd0aa273fb9e304f4e0738d5c8eea1636b18
drewmcd621 <drewmcd621@bitbucket.org>
2012-08-02 17:33:10 -0400

    First

---
+++ Serious.user.js
@@ -0,0 +1,35 @@
+
+// ==UserScript==
+// @name           Project Serious
+// @namespace      http://www.reddit.com/r/SeriouslyLuna
+// @version        0.1
+// @include        http://www.reddit.com/*
+// @include        http://reddit.com/*
+// @include        http://*.reddit.com/*
+// ==/UserScript==
+
+main();
+
+function main()
+{
+    var html = document.getElementsByTagName('body')[0].innerHTML;
+	var search = "seriously";
+	count(html, search);
+}
+
+function count(html, search)
+{
+	int cnt = 0;
+	var regex = new RegExp(search, "img");
+	var matches = [];
+	var match;
+	match = regex.exec(text);
+	while( match instanceof Array  ){
+		cnt++;
+	  matches.push(match);
+	  match = regex.exec(text);
+	}
+	Console.log("Resut:" + cnt);
+	return cnt;
+
+}
